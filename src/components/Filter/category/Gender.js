000b6939133f768dcba528f4252597f518009164
kinb98 <kinjalkbajpai@gmail.com>
2022-01-23 13:03:33 +0530

    Created RickAndMortyWiki

---
+++ src/components/Filter/category/Gender.js
@@ -0,0 +1,44 @@
+import FilterBTN from "../FilterBTN";
+
+const Gender = ({ setGender, setPageNumber }) => {
+  let genders = ["female", "male", "genderless", "unknown"];
+  return (
+    <div className="accordion-item">
+      <h2 className="accordion-header" id="headingThree">
+        <button
+          className="accordion-button collapsed"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#collapseThree"
+          aria-expanded="false"
+          aria-controls="collapseThree"
+        >
+          Gender
+        </button>
+      </h2>
+      <div
+        id="collapseThree"
+        className="accordion-collapse collapse"
+        aria-labelledby="headingThree"
+        data-bs-parent="#accordionExample"
+      >
+        <div className="accordion-body d-flex flex-wrap gap-3">
+          {genders.map((items, index) => {
+            return (
+              <FilterBTN
+                name="gender"
+                index={index}
+                key={index}
+                task={setGender}
+                setPageNumber={setPageNumber}
+                input={items}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Gender;
